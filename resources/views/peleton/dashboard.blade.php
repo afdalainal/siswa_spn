0e048cb7fb6390198f92ee51e8339d888fa0bae1
afdal <afdalainal0@gmail.com>
2025-07-30 23:15:18 +0700

    Update

--- resources/views/peleton/dashboard.blade.php
+++ resources/views/peleton/dashboard.blade.php
@@ -6,7 +6,6 @@
 </div>
 <section class="section">
     <div class="row mb-2">
-
         <div class="col-12 col-md-12">
             <div class="card card-statistic">
                 <div class="card-body p-0">
@@ -24,34 +23,12 @@
                 </div>
             </div>
         </div>
-
     </div>
     <div class="row mb-4">
-
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">
-                    <h3 class='card-heading p-1 pl-3'>Sales</h3>
-                </div>
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-md-4 col-12">
-                            <div class="pl-3">
-                                <h1 class='mt-5'>$21,102</h1>
-                                <p class='text-xs'><span class="text-green"><i data-feather="bar-chart" width="15"></i>
-                                        +19%</span> than last month</p>
-                                <div class="legends">
-                                    <div class="legend d-flex flex-row align-items-center">
-                                        <div class='w-3 h-3 rounded-full bg-info me-2'></div><span class='text-xs'>Last
-                                            Month</span>
-                                    </div>
-                                    <div class="legend d-flex flex-row align-items-center">
-                                        <div class='w-3 h-3 rounded-full bg-blue me-2'></div><span
-                                            class='text-xs'>Current Month</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                         <div class="col-md-8 col-12">
                             <canvas id="bar"></canvas>
                         </div>
@@ -59,7 +36,6 @@
                 </div>
             </div>
         </div>
-
     </div>
 </section>
 @endsection
