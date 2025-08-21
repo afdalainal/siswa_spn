d9ad50b864a8a7bc78db8480fb9bfcf1146f59c2
afdal <afdalainal0@gmail.com>
2025-08-21 20:16:15 +0700

    Update

--- resources/views/layouts/_index.blade.php
+++ resources/views/layouts/_index.blade.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>Sekolah Polisi Negara</title>
+    <title>{{ config('app.name') }}</title>
 
     <link rel="stylesheet" href="{{asset('assets/css/bootstrap.css')}}">
     <link rel="stylesheet" href="{{asset('assets/vendors/simple-datatables/style.css')}}">
